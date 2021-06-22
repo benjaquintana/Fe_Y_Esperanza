bf70b0737cec400928422c6fd53452bad3f46d84
benjaquintana <54126753+benjaquintana@users.noreply.github.com>
2021-06-22 13:01:14 -0700

    1

--- estaciones.php
+++ estaciones.php
@@ -6,26 +6,24 @@
 <section class="seccion seccion_radio">
         <div class="contenedor">
             <h2>Radio en Vivo</h2>
-            <?php
-                try {
+            
+            <ul class="lista_estaciones">
+                <?php try {
                     require_once('includes/funciones/db_conexion.php');
                     $sql = "SELECT link, url_img_radio FROM radios ";
                     $resultado = $conn->query($sql);
                 } catch (\Exception $e) {
                     echo $e->getMessage();
                 }
-            ?>
-            <ul class="contenido_radio clearfix">  
-                <?php while($estacion = $resultado->fetch_assoc() ) { ?>
+                
+                while($estacion = $resultado->fetch_assoc() ) { ?>
                     <!-- Estaciones -->
-                    <li>
-                        <div class="estacion">
-                            <a href="#" target="_blank">
-                                <img src="img/radios/<?php echo $estacion['url_img_radio']; ?>" alt="zeno_radio" title="Nuestra radio en Zeno">
-                            </a>
-                            <audio controls="" src="<?php echo $estacion['link']; ?>" controlslist="nodownload" class="controls"></audio>
-                        </div>
-                    </li>
+                    <div>
+                        <a href="#" target="_blank">
+                            <img src="img/radios/<?php echo $estacion['url_img_radio']; ?>" alt="zeno_radio" title="Nuestra radio en Zeno">
+                        </a>
+                        <audio controls="" src="<?php echo $estacion['link']; ?>" controlslist="nodownload" class="controls"></audio>
+                    </div>
                 <?php } ?>
             </ul>
         </div>
